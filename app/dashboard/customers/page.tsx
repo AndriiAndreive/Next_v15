5a9539be86bb34fa29663b534b1102db7a1499bd
Mohit Sengar <andreievandrii71@gmail.com>
2024-11-01 21:25:22 +0300

    added queryparams

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,4 @@
 import { Metadata } from "next";
-import { lusitana } from '@/app/ui/fonts';
-import Search from "@/app/ui/search";
-import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { Suspense } from "react";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import Table from "@/app/ui/customers/table";
@@ -11,8 +8,16 @@ export const metadata: Metadata = {
     title: 'Customers',
 };
 
-export default async function Page(){
-    const customers = await fetchFilteredCustomers('');
+export default async function Page(props: {
+    searchParams?: Promise<{
+      query?: string;
+      page?: string;
+    }>;
+  }){
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
     return (
         <Suspense fallback={<InvoicesTableSkeleton />}>
             <Table customers={customers} />
